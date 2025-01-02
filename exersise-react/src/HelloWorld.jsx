2014766e7f67b5b2ca5b3ad314765fcafb2a0078
GiovanniTammaro9 <gio_play@live.it>
2025-01-02 16:18:45 +0100

    aggiunto component helloworld

---
+++ exersise-react/src/HelloWorld.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from `react`;
+class HelloWorld extends Component {
+    render() {
+        return (
+           <div>
+            <h1>Hello, World!</h1>
+            <p>questo è un component in react</p>
+           </div>
+        )
+    }
+}
+
+export default HelloWorld;
