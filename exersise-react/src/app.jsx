f77078400c59c4bec69c002880f39b4539c59c95
GiovanniTammaro9 <gio_play@live.it>
2025-01-12 23:16:50 +0100

    3 esercizi sul counter

--- exersise-react/src/app.jsx
+++ exersise-react/src/app.jsx
@@ -1,14 +1,21 @@
 import React from `react`;
 import HelloWorld from './HelloWorld';
 import './app.css'
+import Counter from './Counter';
 
 function App() {
   return (
     <div>
       <HelloWorld /> {/*Aggiungi il componente HelloWorld*/}
+      <Counter />
     </div>
+    
   );
+
+  
 }
+
+
 
 export default App;
 
